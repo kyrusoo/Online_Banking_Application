b7123039ec6f65f3b75b58caf977af3a36f7ff2c
KyrusOo <duzeldikovb@bk.ru>
2025-12-19 21:01:17 +0500

    Tested/Used empty arguments in Bank&Customer.

--- Main.java
+++ Main.java
@@ -7,12 +7,15 @@ public class Main {
 
         Customer customer1 = new Customer("CUST001", "Kairat Nurtas", "kair_n@gmail.com", "8-701-943-3310");
         Customer customer2 = new Customer("CUST002", "Ernar Aidar", "ernar-aidar@mail.ru", "8-727-391-3552");
+        Customer customer_empty = new Customer();
 
         Bank bank = new Bank("MyBank", "MB001");
+        Bank bank_empty = new Bank();
         bank.addCustomer(customer1);
         bank.addCustomer(customer2);
         bank.addAccount(account1);
         bank.addAccount(account2);
+        bank_empty.addAccount(account_empty);
 
         System.out.println("Bank Details:");
         System.out.println(bank);
@@ -20,13 +23,15 @@ public class Main {
         System.out.println(customer1);
         System.out.println("\nCustomer 2:");
         System.out.println(customer2);
+        System.out.println("\n(TESTED)\nEmpty Customer:");
+        System.out.println(customer_empty);
         System.out.println("\nAccount 1:");
         System.out.println(account1);
         System.out.println("\nAccount 2:");
         System.out.println(account2);
         System.out.println("\nAccount 3:");
         System.out.println(account3);
-        System.out.println("\n(Additionally)\nEmpty Account:");
+        System.out.println("\n(TESTED)\nEmpty Account:");
         System.out.println(account_empty);
 
         System.out.println("\nComparisons:");
